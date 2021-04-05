b18a7f7486d7671f0227aa5b1b948775fcd0aab5
Gabriel Osipov <56112831+G4braG4br@users.noreply.github.com>
2021-04-05 12:45:36 +0300

    Update №7.py

--- №7.py
+++ №7.py
@@ -1,18 +1,15 @@
 def combine_anagrams(words_array):
     res = []
-    #temp_array = words_array.split(' ')
     temp_array = ' '.join(words_array).split(' ')
     index_currentWord = 0
     while temp_array != []:
         currentWord = temp_array[0]
-        #print("Проверка слова - ", currentWord)
         temp_array.pop(0)
-        #print(temp_array)
         res.append([currentWord])
         a = set()
         for j in currentWord:
             a.add(j)
-        #print("Сет текущего слова = ", a)
+        lenOfCurWord = len(currentWord)
         g = temp_array.copy()
         # Цикл по словам в temp_array
         te = set()
@@ -20,6 +17,7 @@ def combine_anagrams(words_array):
             # Флаг для проверки на анаграмму
             flag = True
             # Цикл по буквам в слове
+
             for k in range(len(g[i])):
                 # Если хотя бы одна буква не сходится, то пропускаем слово
                 if g[i][k] not in a:
@@ -27,8 +25,7 @@ def combine_anagrams(words_array):
                     flag = False
                     break
             # Слово подошло и его можно исключить из temp_array
-            if flag:
-                #print(f"Слово {g[i]} анаграмма, его индекс в темпе = {i}, удаляем")
+            if flag and lenOfCurWord == len(g[i]):
                 te.add(i)
                 res[index_currentWord].append(g[i])
         temp_array = [temp_array[i] for i in range(len(temp_array)) if i not in te]
