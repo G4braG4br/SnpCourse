132259c07da152650235003c37382d60bd5191f1
Gabriel Osipov <56112831+G4braG4br@users.noreply.github.com>
2021-04-06 23:53:40 +0300

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -1,49 +1,34 @@
+from collections.abc import Iterable
+
+
 def multiply_numbers(*inputs):
     isnum = None
     forint = 1
-
     for i in inputs:
-
-        if type(i) is list or type(i) is tuple or type(i) is set:
+        if isinstance(i, Iterable):
             for el in i:
-
-                if type(el) is int:
-                    forint *= el
-                    isnum = forint
-                elif type(el) is float:
+                if isinstance(el, str):
+                    for char in el:
+                        if char.isdigit():
+                            forint *= int(char)
+                            isnum = forint
+                else:
                     temp = multiply_numbers(el)
-                    forint *= temp
-                    isnum = forint
-                elif type(el) is list:
-                    temp = multiply_numbers(el)
-                    forint *= temp
-                    isnum = forint
+                    if isinstance(temp, int) or isinstance(temp, float):
+                        forint *= temp
+                        isnum = forint
 
-        if type(i) is int:
+        elif type(i) is int:
             forint *= i
             isnum = forint
 
-        if type(i) is float:
-            lis = str(i)
-            for el in lis:
-                if el.isdigit():
-                    forint *= int(el)
-                    isnum = forint
-
-        if type(i) is str:
-            for el in i:
-                if el.isdigit():
-                    forint *= int(el)
-                    isnum = forint
-
+        elif type(i) is float:
+            temp = multiply_numbers(str(i))
+            forint *= temp
+            isnum = forint
+            
     return isnum
 
+#print(multiply_numbers([[[[[2]]]], "@34", 3, [2.2, 2], (2, 3.3), {2, 3}])) # => 62 208
+#print(multiply_numbers({2: 3}))
 
-
-print(multiply_numbers()) # => None
-print(multiply_numbers('ss')) # => None
-print(multiply_numbers('1234')) # => 24
-print(multiply_numbers('sssdd34')) # => 12
-print(multiply_numbers(2.3)) # => 6
-print(multiply_numbers([5, 6, 4])) # => 120
-print(multiply_numbers([[[[2]]]], "@34", 3, [2.2, 2], (2, 3.3), {2, 3})) # => 62 208
