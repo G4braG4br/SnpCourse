e426c9cfe08be9341646874fa3cc3ff33c7ef099
Gabriel Osipov <56112831+G4braG4br@users.noreply.github.com>
2021-04-06 11:46:21 +0300

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -46,4 +46,4 @@ print(multiply_numbers('1234')) # => 24
 print(multiply_numbers('sssdd34')) # => 12
 print(multiply_numbers(2.3)) # => 6
 print(multiply_numbers([5, 6, 4])) # => 120
-print(multiply_numbers(1, [[[3]]], "@34", 3, [2.2, 2], (2, 3.3), {2, 3})) # => 93 312
+print(multiply_numbers([[[[2]]]], "@34", 3, [2.2, 2], (2, 3.3), {2, 3})) # => 62 208
