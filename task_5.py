5e6d2b7d43cc0a3826f58c3a935eb72181f61916
Gabriel Osipov <56112831+G4braG4br@users.noreply.github.com>
2021-04-06 11:01:52 +0300

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -2,13 +2,12 @@ import datetime
 
 def date_in_future(integer: int=None):
     if integer is None:
-        return None
+        return f"{datetime.datetime.now():%d-%m-%Y %H:%M:%S}"
     dt_now = datetime.datetime.now()
     if type(integer) != int:
-        return f"{datetime.datetime.now():%d-%m-%y %H:%M:%S}"
+        return f"{datetime.datetime.now():%d-%m-%Y %H:%M:%S}"
     res = dt_now + datetime.timedelta(days=integer)
-    #res = datetime.datetime(res.date().year, month=integer, day=res.date().day)
-    return f"{res:%d-%m-%y %H:%M:%S}"
+    return f"{res:%d-%m-%Y %H:%M:%S}"
 
 
 """
