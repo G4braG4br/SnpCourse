a333e06f88efb2ae551dee7b530d0c9bb5eb15ec
Gabriel Osipov <56112831+G4braG4br@users.noreply.github.com>
2021-04-15 20:22:18 +0300

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -20,18 +20,20 @@ def multiply_numbers(*inputs):
                         isnum = forint
 
         elif type(i) is int:
-            forint *= i
+            temp = multiply_numbers(str(i))
+            forint *= temp
             isnum = forint
 
         elif type(i) is float:
             temp = multiply_numbers(str(i))
             forint *= temp
             isnum = forint
-            
+
     return isnum
 
+
 def multiply_numbers2(*args):
-    return reduce(lambda x, y: x * y, list(filter(lambda x: x!= "@", list(map(lambda x: int(x) if x.isdigit() else "@", str(args)))))) if list(filter(lambda x: x!= "@", list(map(lambda x: int(x) if x.isdigit() else "@", str(args))))) != [] else None
+    return reduce(lambda x, y: x * y, list(int(i) for i in str(args) if i.isdigit()) ) if ( True in list(map(lambda x: x.isdigit(), str(args))) != [] ) else  None
 
 
 #print(multiply_numbers([[[[[2]]]], "@34", 3, [2.2, 2], (2, 3.3), {2, 3}])) # => 62 208
